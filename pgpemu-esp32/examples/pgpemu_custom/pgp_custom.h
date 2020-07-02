94efc97a789b374b8cae9cfdf9b012d48b3bb9fe
James Hawtin <oolon@ankh.org>
2020-07-02 16:26:51 +0100

    Improving the customisation example.

--- pgpemu-esp32/examples/pgpemu_custom/pgp_custom.h
+++
@@ -1,7 +0,0 @@
-#ifndef __pgpemu_custom_h__
-#define __pgpemu_custom_h__
-
-// #define PGPEMU_SPLASH     my_jpg
-// #define PGPEMU_SPLASH_LEN my_jpg_len
-
-#endif
---
+++ pgpemu-esp32/examples/pgpemu_custom/pgpemu_custom.h
@@ -0,0 +1,12 @@
+#ifndef __pgpemu_custom_h__
+#define __pgpemu_custom_h__
+
+#include <stdint.h>
+
+extern uint8_t my_jpg[];
+unsigned int my_jpg_len;
+
+#define PGPEMU_SPLASH     my_jpg
+#define PGPEMU_SPLASH_LEN my_jpg_len
+
+#endif
